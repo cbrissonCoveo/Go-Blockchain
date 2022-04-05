f020c00cc406c68034a05e249cd2040c24658717
Cedric Brisson <cbrisson@coveo.com>
2022-04-04 20:57:10 -0400

    Part 2 and 3: PoW, persistant storage & CLI interface

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/cbrissonCoveo/Go-Blockchain
 
 go 1.18
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20220403205710-6acee93ad0eb // indirect
+)
---
+++ serialization.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		panic(err)
+	}
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &block
+}
